77c6e4f7fb67032063308d010eb34e90a43b54e4
gabrielfagundeznievas <gabrielfagundeznievas@gmail.com>
2022-08-28 01:10:58 -0300

    Funcionalidad a los filtros

--- js/product-info.js
+++ js/product-info.js
@@ -2,6 +2,7 @@ async function showProducts() {
     try {
       const result = await fetch(PRODUCTS_URL + localStorage.catID + '.json');
       const data = await result.json();
+      
     }
     catch (error){
         console.log(error);
--- js/products.js
+++ js/products.js
@@ -1,26 +1,64 @@
-async function showProducts() {
-  try {
-    const result = await fetch(PRODUCTS_URL + localStorage.catID + '.json');
-    const data = await result.json();
-    const catName = document.getElementById('catName');
-    const sectionProduct = document.getElementById('product');
+document.addEventListener('DOMContentLoaded', () => {
+  async function showProducts() {
+    try {
+      const result = await fetch(PRODUCTS_URL + localStorage.catID + '.json');
+      const data = await result.json();
+      const catName = document.getElementById('catName');
+      const sectionProduct = document.getElementById('product');
+      const sortAsc = document.getElementById('sortAsc');
+      const sortDesc = document.getElementById('sortDesc');
+      const sortByRel = document.getElementById('sortByRel');
+  
+      catName.textContent = data.catName;
+  
+      sortAsc.addEventListener('click', () => {
+        data.products = data.products.sort((a, b) => b.cost - a.cost);
 
-    catName.textContent = data.catName;
+        sectionProduct.innerHTML = "";
+        cardProducts();
+      });
 
-    for (product of data.products) {
-      sectionProduct.innerHTML += `<a href='product-info.html' id="product${product.id}" class="card card_product shadow-sm mb-5 bg-body rounded" style="width: 18rem;">
-        <img src="${product.image}" class="card-img-top" alt="foto de producto">
-        <div class="card-body">
-          <p class="card-text fs-3">${product.currency} ${product.cost}</p>
-          <p class="card-text fw-bold">${product.name}</p>
-          <p class="card-text fw-light">${product.description}</p>
-        </div>
-      </a>`
+      sortDesc.addEventListener('click', () => {
+        data.products = data.products.sort((a, b) => a.cost - b.cost);
 
-    };
-  } catch (error) {
-    console.log(error);
+        sectionProduct.innerHTML = "";
+        cardProducts();
+      });
+
+      sortByRel.addEventListener('click', () => {
+        data.products = data.products.sort((a, b) => b.soldCount - a.soldCount);
+
+        sectionProduct.innerHTML = "";
+        cardProducts();
+      });
+
+      function cardProducts(){
+        for (product of data.products) {
+          sectionProduct.innerHTML += `<div onclick="setCatID(${product.id})" class="card card_product shadow-sm mb-5 bg-body rounded cursor-active" style="width: 18rem;">
+            <img src="${product.image}" class="card-img-top" alt="foto de producto">
+            <div class="card-body">
+              <p class="card-text fs-3">${product.currency} ${product.cost}</p>
+              <p class="card-text fw-bold">${product.name}</p>
+              <p class="card-text fw-light">${product.description}</p>
+              <p class="card-text fw-lighter text-end">${product.soldCount} vendidos</p>
+            </div>
+          </div>`
+        };
+      };
+
+      cardProducts();
+      
+    } catch (error) {
+      console.log(error);
+    }
   }
+
+  showProducts();
+})
+
+function setCatID(id) {
+  localStorage.setItem("catID", id);
+  window.location = "product-info.html";
 }
 
 function login(){
@@ -38,6 +76,7 @@ function logout(){
   })
 }
 
+
+
 login()
 logout()
-showProducts();
